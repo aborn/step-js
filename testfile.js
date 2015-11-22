e5c35a25fc8660496ad26a9379a9ddde24048127
guobao.jiang <loveaborn@foxmail.com>
2015-11-23 00:27:11 +0800

    update

--- testfile.js
+++ testfile.js
@@ -2,3 +2,13 @@
 
 var path  = require('path');
 var fs = require('fs');
+
+var codePath = path.join(process.env.HOME, "dpcode");
+console.log(codePath);
+var filesDir = fs.readdirSync(codePath);
+
+filesDir.map(function(filename, index) {
+    if (filename.indexOf("mapi") == 0) {
+        console.log(filename + index);
+    }
+})
