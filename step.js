dcf5ac8951b709a760a451071b09d43c5ff66065
蒋国宝 <loveaborn@foxmail.com>
2015-11-24 23:35:46 +0800

    update

--- step.js
+++ step.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
 console.log("step-js");
+
+function testArguments(arg0, arg1) {
+    console.log('-------------');
+    console.log(arguments.callee);
+    console.log('need argument number:' + arguments.callee.length);
+    console.log('actual input arg number:' + arguments.length);
+}
+
+testArguments('a');
+testArguments();
+testArguments('a', 1, 3);
